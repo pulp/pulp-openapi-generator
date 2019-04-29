f1ff8a1fb94df35ddf48d3f5a369f5b60d22ffe2
Dennis Kliban <dkliban@redhat.com>
2019-04-29 16:51:33 -0400

    Fix [[ to [

--- generate.sh
+++ generate.sh
@@ -8,7 +8,7 @@ curl -o api.json http://localhost:24817/pulp/api/v3/docs/api.json?plugin=$1
 # Get the version of the pulpcore or plugin as reported by status API
 export VERSION=$(http :24817/pulp/api/v3/status/ | jq --arg plugin $1 -r '.versions[] | select(.component == $plugin) | .version')
 
-if [[ $3 && ${3-x} ]];
+if [ ${3-x} ];
 then
     export VERSION=$VERSION-$3
 fi
