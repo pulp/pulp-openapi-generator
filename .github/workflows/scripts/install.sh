cf44cbd87526f4bfc78541a2dd1ec46177555fc9
Daniel Alley <dalley@redhat.com>
2020-12-01 15:32:15 -0500

    Fix Github Actions
    [noissue]

--- .github/workflows/scripts/install.sh
+++ .github/workflows/scripts/install.sh
@@ -9,21 +9,13 @@
 
 set -euv
 
-if [ "$TEST" = "docs" ]; then
-  pip install -r ../pulpcore/doc_requirements.txt
-  pip install -r doc_requirements.txt
-fi
-
-pip install -r functest_requirements.txt
+mkdir .ci/ansible/vars || true
+echo "---" > .ci/ansible/vars/main.yaml
 
 cd .ci/ansible/
 
 TAG=ci_build
-if [[ "$TEST" == "plugin-from-pypi" ]]; then
-  PLUGIN_NAME=pulp_file
-else
-  PLUGIN_NAME=./pulp_file
-fi
+
 if [ -n "${GITHUB_REF##*/}" ]; then
   # Install the plugin only and use published PyPI packages for the rest
   # Quoting ${TAG} ensures Ansible casts the tag as a string.
@@ -35,7 +27,7 @@ plugins:
   - name: pulpcore
     source: pulpcore
   - name: pulp_file
-    source:  "${PLUGIN_NAME}"
+    source:  "pulp_file"
 services:
   - name: pulp
     image: "pulp:${TAG}"
@@ -49,7 +41,7 @@ image:
   tag: "${TAG}"
 plugins:
   - name: pulp_file
-    source: "${PLUGIN_NAME}"
+    source: "pulp_file"
   - name: pulpcore
     source: ./pulpcore
 services:
@@ -63,28 +55,6 @@ fi
 cat >> vars/main.yaml << VARSYAML
 pulp_settings: {"allowed_content_checksums": ["sha1", "sha224", "sha256", "sha384", "sha512"], "allowed_export_paths": ["/tmp"], "allowed_import_paths": ["/tmp"]}
 VARSYAML
-
-if [[ "$TEST" == "pulp" || "$TEST" == "performance" || "$TEST" == "s3" || "$TEST" == "plugin-from-pypi" ]]; then
-  sed -i -e '/^services:/a \
-  - name: pulp-fixtures\
-    image: docker.io/pulp/pulp-fixtures:latest\
-    env: {BASE_URL: "http://pulp-fixtures"}' vars/main.yaml
-fi
-
-if [ "$TEST" = "s3" ]; then
-  export MINIO_ACCESS_KEY=AKIAIT2Z5TDYPX3ARJBA
-  export MINIO_SECRET_KEY=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
-  sed -i -e '/^services:/a \
-  - name: minio\
-    image: minio/minio\
-    env:\
-      MINIO_ACCESS_KEY: "'$MINIO_ACCESS_KEY'"\
-      MINIO_SECRET_KEY: "'$MINIO_SECRET_KEY'"\
-    command: "server /data"' vars/main.yaml
-  sed -i -e '$a s3_test: true\
-minio_access_key: "'$MINIO_ACCESS_KEY'"\
-minio_secret_key: "'$MINIO_SECRET_KEY'"' vars/main.yaml
-fi
 
 ansible-playbook build_container.yaml
 ansible-playbook start_container.yaml
